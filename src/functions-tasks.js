23bf0ff6f5820d21224867142a322f02056b3019
unknown <boss.redgar@mail.ru>
2025-04-14 21:22:39 +0300

    feat: add 8 task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -213,7 +213,9 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
 function partialUsingArguments(fn, ...args1) {
-
+  return function (...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
